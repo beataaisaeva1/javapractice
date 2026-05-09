265916b38ccbaa58bfb0606d27b745c4c903c5bb
beataaisaeva1 <beataaisaeva1488@gmail.com>
2026-05-09 20:46:40 +0300

    Add files via upload

---
+++ practice3_2.java
@@ -0,0 +1,5 @@
+public class practice3_2 {
+    public static void main(String[] args) {
+        System.out.println("\u0411\u0435\u0430\u0442\u0440\u0438\u0441\u0441\u0430");
+    }
+}
---
+++ practice3_4.java
@@ -0,0 +1,15 @@
+public class practice3_4 {
+    public static void main(String[] args) {
+        String s1 = "Возраст: " + 20;
+        String s2 = "Цена: " + 15.5;
+        String s3 = "Статус: " + true;
+        String s4 = "Символ: " + 'A';
+        Integer number = 100;
+        String s5 = "Число: " + number;
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+    }
+}
