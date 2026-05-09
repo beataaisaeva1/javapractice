3a5b348f7f5ef958d1af7e6eccfb841be8ae4d4c
beataaisaeva1 <beataaisaeva1488@gmail.com>
2026-05-09 19:44:58 +0300

    Add files via upload

---
+++ practice3.java
@@ -0,0 +1,37 @@
+public class practice3 {
+
+    public static void main(String[] args) {
+        byte b = 10;
+        byte b0 = 0;
+        short s = 1000;
+        short s0 = 0;
+        int i = 50000;
+        int i0 = 0;
+        long l = 100000L;
+        long l0 = 0L;
+
+        float f = 5.5f;
+        float f0 = 0.0f;
+        double d = 10.99;
+        double d0 = 0.0;
+
+        char c = 'A';
+        char c0 = '\u0000'; 
+
+        boolean bool = true;
+        boolean bool0 = false;
+
+        String str = "Привет";
+        String str0 = null;
+
+        System.out.println(b + " " + b0);
+        System.out.println(s + " " + s0);
+        System.out.println(i + " " + i0);
+        System.out.println(l + " " + l0);
+        System.out.println(f + " " + f0);
+        System.out.println(d + " " + d0);
+        System.out.println(c + " " + c0);
+        System.out.println(bool + " " + bool0);
+        System.out.println(str + " " + str0);
+    }
+}
